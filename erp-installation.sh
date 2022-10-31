759a31a36d3536c4ad7224772a40e80ba23cf7f1
Ranjithkumar Sekar <103019099+Ranjith1022@users.noreply.github.com>
2022-10-31 10:11:57 +0530

    Update erp-installation.sh

--- erp-installation.sh
+++ erp-installation.sh
@@ -1,5 +1,3 @@
-
-
 ############################---Description---###################################
 #                                                                              #
 # Summary       : Frappe-Framework with ERPNext                                #
@@ -11,40 +9,9 @@
 
 set -e
 
-printf "${white}
-
-############################---Description---###################################
-#                                                                              #
-# Summary       : Frappe-Framework with ERPNext                                #
-# Support       : ranjithks@am.amrita.edu                                      #
-# Created date  : Jul 21,2022                                                  #
-# Latest Modified date : Oct 20,2022                                           #
-#                                                                              #
-################################################################################
-
-${clear}
-
-"
-
-############################---Usage---##########################################
-#										#
-# Accounting									#
-# Warehouse Management								#
-# CRM										#
-# Sales										#
-# Purchase									#
-# HRMS										#
-# Project Management								#
-# Support									#
-# Asset Management								#
-# Quality Management								#
-# Manufacturing									#
-# Website Management								#
-# Customize ERPNext								#
-# And More									#
-#										#
-#################################################################################
-
+# Color variables
+#A script can use escape sequences to produce colored text on the terminal. Colors for text are represented by color codes,
+#including, reset = 0, black = 30, red = 31, green = 32, yellow = 33, blue = 34, magenta = 35, cyan = 36, and white = 37.
 
 red='\033[0;31m'
 green='\033[0;32m'
@@ -55,6 +22,40 @@ cyan='\033[0;36m'
 white='\033[0;37m'
 # Clear the color after that
 clear='\033[0m'
+
+
+printf "${white}
+############################---Description---####################################
+#                                                                               #
+# Summary               : Frappe-Framework with ERPNext                         #
+# Support               : ${yellow}ranjithks@am.amrita.edu${clear}                               #
+# Created date          : Jul 21,2022                                           #
+# Latest Modified date  : Oct 20,2022                                           #
+# Created by            : ${blue}RANJITHKUMAR SEKAR${clear}                                     #
+#                                                                               #
+#################################################################################
+${clear}
+"
+
+############################---Usage---##########################################
+#                                                                               #
+# Accounting                                                                    #
+# Warehouse Management                                                          #
+# CRM                                                                           #
+# Sales                                                                         #
+# Purchase                                                                      #
+# HRMS                                                                          #
+# Project Management                                                            #
+# Support                                                                       #
+# Asset Management                                                              #
+# Quality Management                                                            #
+# Manufacturing                                                                 #
+# Website Management                                                            #
+# Customize ERPNext                                                             #
+# And More                                                                      #
+#                                                                               #
+#################################################################################
+
 #bold colors
 bld_red='\033[1;31m'
 bld_green='\033[1;32m'
@@ -73,7 +74,11 @@ bg_cyan='\033[0;46m'
 #text bold
 bold=`tput bold`
 offbold=`tput rmso`
-
+#Frappe, pronounced fra-pay, is a full stack, batteries-included, web framework written in Python and Javascript with MariaDB as the database.
+#It is the framework which powers ERPNext. It is pretty generic and can be used to build database driven apps.
+#The key difference in Frappe compared to other frameworks is that meta-data is also treated as data and is used to build front-ends very easily.
+#We believe in a monolithic architecture, so Frappe comes with almost everything you need to build a modern web application.
+#It has a full featured Admin UI called the Desk that handles forms, navigation, lists, menus, permissions, file attachment and much more out of the box.
 function entering()
 {
     PARAM_PROGRESS=$1;
@@ -107,16 +112,14 @@ function entering()
 
 function delay()
 {
-    sleep 0.5;
+    sleep 0.2;
 }
-
 
 printf  "${green}
  ________________________________________________________________________
 |									 |
 |                           ENTERING SCRIPT				 |
 |________________________________________________________________________|
-
 ${clear}\n"
 
 CURRENT_PROGRESS=0
@@ -131,13 +134,23 @@ entering 100 "Entered                  "
 
 
 echo
+#The sudo apt-get update command is used to download package information from all configured sources.
+#The sources often defined in the /etc/apt/sources. list file and other files located in /etc/apt/sources.
 
-
-sudo apt-get update -y
-
+#upgrade upgrade is used to install the newest versions of all packages currently installed on the system from the sources enumerated in /etc/apt/sources. list.
 sudo apt-get upgrade -y
-
+#The autoremove option removes packages that were automatically installed because some other package required them but, with those other packages removed, they are no longer needed.
+#Sometimes, an upgrade will suggest that you run this command.
 sudo apt autoremove -y
+
+#This guide assumes you are using a personal computer, VPS or a bare-metal server.
+#You also need to be on a *nix system, so any Linux Distribution and MacOS is supported. However, we officially support only the following distributions.
+printf "${yellow}Installing nodejs${clear}\n"
+sudo apt install -y curl
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+#Now you will add MariaDB to your server stack. ERPNext 12 requires MariaDB 10.2+ for proper operation.
+#Because Ubuntu 20.04 includes MariaDB 10.3 in its official repositories you can install this version using the apt command:
+#First, you will need to install Python and other packages required to build and set up Frappe framework.
 
 printf "${bld_yellow}
 
@@ -145,28 +158,19 @@ Installing Resources
 
 ${clear}"
 
-
-sudo apt install -y curl
-sudo curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt update 
 sudo apt install -y nodejs mariadb-server redis-server python3-pip nginx python3-testresources
-sudo npm install -g yarn
-python3 -V > python-version.txt
-search=`grep --color 3.10 python-version.txt`
+printf "${bold}${green}
 
-if ${search}
-then
-        sudo apt install python3.10-venv -y
-else
-        sudo apt install python3.8-venv -y
-fi
+Resources Installed...!
 
-
+${offblod}${clear}\n"
+#Once the MariaDB is secured, log in to the MariaDB console with the following command
+#Next, you will need to change MariaDB Innodb file format to Barracuda. You can configure it by editing the file
+sudo systemctl stop mysql
 sudo systemctl set-environment MYSQLD_OPTS="--skip-networking --skip-grant-tables"
-
+sudo systemctl start mysql
 
 printf "${green}
-
  _____________________________
 |                             |
 |       ---------             |
@@ -174,13 +178,11 @@ printf "${green}
 |       ---------             |
 |_____________________________|
 
-
 ${clear}"
-
 
 sudo mysql -uroot -p << EOF
 flush privileges;
-alter user root@localhost identified by 'Erp@123'
+alter user root@localhost identified by 'Erp@123';
 flush privileges;
 EOF
 
@@ -200,22 +202,18 @@ printf "
 # One can use all long options that the program supports.
 # Run program with --help to get a list of available options and with
 # --print-defaults to see which it would actually understand and use.
-
 #
 # This group is read both both by the client and the server
 # use it for options that affect everything
 #
 [client-server]
-
 # Import all .cnf files from configuration directory
 !includedir /etc/mysql/conf.d/
 !includedir /etc/mysql/mariadb.conf.d/
-
 [mysqld]
 character-set-client-handshake = FALSE
 character-set-server = utf8mb4
 collation-server = utf8mb4_unicode_ci
-
 [mysql]
 default-character-set = utf8mb4
 " >  /etc/mysql/my.cnf
@@ -230,71 +228,40 @@ $ _________________________$ ${clear}
 "
 sudo systemctl start mysql.service
 
+
+#It is recommended to install Yarn through the npm package manager, which comes bundled with Node.js when you install it on your system.
+sudo npm install -g yarn
+printf "${yellow}Installing Frappe-Bench${clear}\n"
+#We recommend using either the Docker Installation or the Easy Install Script to setup a Production Environment.
+#For Development, you may choose either of the three methods to setup an instance.
 sudo pip3 install frappe-bench
-
-
-
-function progress()
-{
-    PARAM_PROGRESS=$1;
-    PARAM_PHASE=$2;
-
-    if [ $CURRENT_PROGRESS -le 0 -a $PARAM_PROGRESS -ge 0 ]  ; then echo -ne "[..........................] (0%)  $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 5 -a $PARAM_PROGRESS -ge 5 ]  ; then echo -ne "[#.........................] (5%)  $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 10 -a $PARAM_PROGRESS -ge 10 ]; then echo -ne "[##........................] (10%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 15 -a $PARAM_PROGRESS -ge 15 ]; then echo -ne "[###.......................] (15%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 20 -a $PARAM_PROGRESS -ge 20 ]; then echo -ne "[####......................] (20%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 25 -a $PARAM_PROGRESS -ge 25 ]; then echo -ne "[#####.....................] (25%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 30 -a $PARAM_PROGRESS -ge 30 ]; then echo -ne "[######....................] (30%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 35 -a $PARAM_PROGRESS -ge 35 ]; then echo -ne "[#######...................] (35%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 40 -a $PARAM_PROGRESS -ge 40 ]; then echo -ne "[########..................] (40%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 45 -a $PARAM_PROGRESS -ge 45 ]; then echo -ne "[#########.................] (45%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 50 -a $PARAM_PROGRESS -ge 50 ]; then echo -ne "[##########................] (50%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 55 -a $PARAM_PROGRESS -ge 55 ]; then echo -ne "[###########...............] (55%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 60 -a $PARAM_PROGRESS -ge 60 ]; then echo -ne "[############..............] (60%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 65 -a $PARAM_PROGRESS -ge 65 ]; then echo -ne "[#############.............] (65%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 70 -a $PARAM_PROGRESS -ge 70 ]; then echo -ne "[###############...........] (70%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 75 -a $PARAM_PROGRESS -ge 75 ]; then echo -ne "[#################.........] (75%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 80 -a $PARAM_PROGRESS -ge 80 ]; then echo -ne "[####################......] (80%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 85 -a $PARAM_PROGRESS -ge 85 ]; then echo -ne "[#######################...] (85%) $PARAM_PHASE \r"  ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 90 -a $PARAM_PROGRESS -ge 90 ]; then echo -ne "[##########################] (100%) $PARAM_PHASE \r" ; delay; fi;
-    if [ $CURRENT_PROGRESS -le 100 -a $PARAM_PROGRESS -ge 100 ];then echo -ne 'Project Started!                                \n' ; delay; fi;
-
-    CURRENT_PROGRESS=$PARAM_PROGRESS;
-
-}
-
-
 sudo systemctl restart mariadb
-
 sudo pip3 install frappe-bench
-
+printf "${green}Installing bench components...!${clear}\n"
 sudo -H pip3 install frappe-bench
-
-printf "${yellow}bench Installing...!${clear}"
+#Git is a free, open-source, distributed version control system that handles source code changes in software projects of all sizes.
+#Git allows multiple developers to work together on the same project with ease
+sudo apt install git -y
+printf "${green}...${clear}"
+#Initialize the bench directory with Frappe framework using the following command
 cd ~
-bench init --frappe-branch version-13 frappe-framework
-
-cd frappe-framework
-
-bench new-site erp-site
-bench use erp-site
-
+bench init --frappe-branch version-14 frappe-bench
+#change the directory to erpnext and create a new frappe site with the following command
+cd frappe-bench
+#Provide your password and hit Enter to create a new Frappe site
+bench new-site frappe-site
+bench use frappe-site
 sudo service nginx stop
-
 cd ~
 cd frappe-bench
-bench get-app --branch version-13 erpnext
-bench --site erp-site install-app erpnex
-bench use erp-site
 printf "
 ${bld_green}
 
 (Project Strating-:)...)
+
 ${clear}
 "
-
-
+bench use frappe-site
 
 printf "${yellow}
 ####################---Project Details---###################
@@ -311,15 +278,12 @@ ${clear}
 |                                                                             |
 | ${green}# Application Username: ${clear}${red} Administrator${clear}                                |
 |_____________________________________________________________________________|
-
-
 ${red}Notes:${clear} ${blue}[Please check the ${clear} ${magenta}'/home/frappe-framework/Project_Details.txt'${clear}${blue} file for above Project Details]${clear}\n
 "
 
 
 printf "
 ####################---Project Details---###################
-
  _____________________________________________________________________________
 |                                                                             |
 | # Project Path: /home/frappe-framework                                      |
@@ -347,6 +311,12 @@ progress 90 "Finalizing...          "
 progress 100 "Done                  "
 
 echo
+
+cd ~
+cd frappe-bench
+bench get-app --branch version-14 erpnext
+bench --site erp-site install-app erpnex
+bench use erp-site
 
 
 bench start
